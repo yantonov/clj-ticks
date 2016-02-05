582ddd38b55aeacaef6c41a6acdc6d6aa4c776ed
Yury Antonov <yantonov@yandex.ru>
2016-02-05 07:56:12 +0500

    bump up clojure version and fix project settings

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-ticks "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "convert c# ticks to date and vise versa"
+  :url "https://github.com/yantonov/ticks"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]])
