63b3cc14ae9fe9a0838c4cc0c305b18e18577794
Yury Antonov <yantonov@yandex.ru>
2017-11-04 04:15:43 +0500

    bumped up cljs version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.7.228"]]
+                 [org.clojure/clojurescript "1.9.946"]]
 
   :plugins
   [[lein-cljsbuild "1.1.2"]]
